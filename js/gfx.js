94295d512d12f60405d96bb1591368665cb2c2c0
maximinus <maximinus@gmail.com>
2014-05-13 16:47:51 +0800

    Fractal scales to screen

--- js/gfx.js
+++ js/gfx.js
@@ -13,6 +13,7 @@ function GFXEngine() {
 			this.context = this.canvas.getContext('2d'); }
 		this.image = this.context.getImageData(0, 0, this.width, this.height);
 		this.image_data = this.image.data;
+		this.colour_band = getColourBanding();
 	};
 
 	// class to handle all website and screen interactions
@@ -34,9 +35,31 @@ function GFXEngine() {
 	};
 	
 	this.drawMandelbrot = function(x0, x1, y0, y1) {
-		// draw from -1 to +1 on the y scale
-		var xscale	= (x1 - x0) / this.width;
-		var yscale = (y1 - y0) / this.height;
+		// we might have to stretch things a bit
+		var scale = 0;
+		//if(((x1 - x0) / this.width) < ((y1 - y0) / this.height)) {
+		if(true) {
+			console.log('x');
+		
+			// xscale is smaller, so has more room. True scale is now y
+			scale = ((y1 - y0) / this.height) * -1;
+			// resize x
+			var extra_size = ((this.width * scale) - (x1 - x0)) / 2.0;
+			x0 -= extra_size;
+			x1 += extra_size;
+		}
+		else {
+		
+			console.log('y');
+		
+			// yscale is smaller, so has more room. True scale is now x
+			scale = (x1 - x0) / this.width;
+			// resize y
+			var extra_size = ((this.height * scale) - (y1 - y0)) / 2.0;
+			y0 -= extra_size;
+			y1 += extra_size;
+		}
+		var xstart = x0;
 		for(var ypos=0; ypos<this.height; ypos++) {
 			for(var xpos=0; xpos<this.width; xpos++) {
 				var x = 0.0;
@@ -44,28 +67,54 @@ function GFXEngine() {
 				var xtemp = 0.0;
 				var ytemp = 0.0;
 				var iters = 0;
-				while(((x * x) + (y * y) < 4.0) && (iters<255)) {
+				while(((x * x) + (y * y) < 4.0) && (iters<156)) {
 					xtemp = (x * x) - (y * y) + x0;
 					y = 2 * x * y + y0;
 					x = xtemp;
 					iters += 1;
 				}
-				this.plotPixel(xpos, ypos, iters);
-				x0 += xscale;
+				this.plotPixel(xpos, ypos, this.colour_band[iters]);
+				x0 += scale;
 			}
-			x0 = -2.0;
-			y0 += yscale;
+			x0 = xstart;
+			y0 -= scale;
 		}
 		console.log('finished');
 		this.context.putImageData(this.image, 0, 0);
 	};
 	
 	this.plotPixel = function(x, y, value) {
+		//console.log(value);
+	
 		var index = (y * this.width + x) * 4;
-		this.image_data[index++] = value;
-		this.image_data[index++] = value;
-		this.image_data[index++] = value;
+		this.image_data[index++] = value[0];
+		this.image_data[index++] = value[1];
+		this.image_data[index++] = value[2];
 		this.image_data[index] = 255;
 	};
+};
+
+function getColourBanding() {
+	// return an array of colour values
+	var colours = new Array();
+	var count = 10;
+	var max_count = 250
+	var red = max_count;
+	var green = 0;
+	var blue = 0;
+	for(green = 0; green<=max_count; green+=count) {
+		colours.push([red, green, blue]); }
+	for(red = max_count; red>=0; red-=count) {
+		colours.push([red, green, blue]); }
+	for(blue = 0; blue<=max_count; blue+=count) {
+		colours.push([red, green, blue]); }
+	for(green = max_count; green>=0; green-=count) {
+		colours.push([red, green, blue]); }
+	for(red = 0; red<=max_count; red+=count) {
+		colours.push([red, green, blue]); }
+	for(blue = max_count; blue>=0; blue-=count) {
+		colours.push([red, green, blue]); }
+	colours.push([0, 0, 0]);
+	return(colours);
 };
 
