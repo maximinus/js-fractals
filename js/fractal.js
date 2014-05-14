593a8fafb5f437acc6ea459d20e97290d21a69e8
maximinus <maximinus@gmail.com>
2014-05-14 17:42:10 +0800

    Refactoring to use objects

--- js/fractal.js
+++ js/fractal.js
@@ -9,7 +9,7 @@ function Fractal() {
 	this.init = function() {
 		this.gfx = new GFXEngine();
 		this.gfx.init();
-		this.gfx.drawMandelbrot(-2.0, 1.0, 1.0, -1.0);
+		this.gfx.drawMandelbrot();
 	};
 };
 
